92aeca1ecef938ba4bb168f913dbef58c1c429fd
Amanskn <29maddhesiaaman@gmail.com>
2023-12-18 15:36:31 +0530

    made the db connection in index.js itself

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import {DB_NAME} from './constants.js'
+import express from "express";
+
+const app = express();
 
 
-console.log("Inside index.js again", process.env.PORT)
+;(async () => {
+  try {
+    const dbConnection = await mongoose.connect(process.env.MONGO_URI,{
+      dbName:DB_NAME,
+    })    
+    console.log("Connected to the DB !!!!!!!!!!!",dbConnection.connection.host)
+    app.on("error", (error)=>{
+      console.log("Error in running server @@@:-",error.message)
+      throw error;
+    })
+
+    app.listen(process.env.PORT,()=>{
+      console.log("@@@ App is listening on port",process.env.PORT);
+
+    })
+  } catch (error) {
+    console.error("Error@@@@@@@@@", error);
+    throw error;
+    // return;
+    // process.exit(1);
+  }
+})();
+
