ccadef5272978fb353ce6e6751b4b3cffccb2182
Amanskn <29maddhesiaaman@gmail.com>
2023-12-18 16:40:46 +0530

    made the db connecion separately

---
+++ src/db/connect-db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+export const connectDb = async () => {
+    try {
+      const dbConnection = await mongoose.connect(process.env.MONGO_URI,{
+        dbName:DB_NAME,
+      })    
+      console.log("!!! Connected to the DB @ host:",dbConnection.connection.host)
+    } catch (error) {
+      console.error("Error@@@@@@@@@", error.message);
+      // throw error;
+      process.exit(1);
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,32 +1,50 @@
 import dotenv from "dotenv";
-dotenv.config();
-import mongoose from "mongoose";
-import {DB_NAME} from './constants.js'
+dotenv.config({
+  path: "./.env",
+});
+
+
 import express from "express";
+import { connectDb } from "./db/connect-db.js";
 
-const app = express();
-
+const app = express()
 
 ;(async () => {
   try {
-    const dbConnection = await mongoose.connect(process.env.MONGO_URI,{
-      dbName:DB_NAME,
-    })    
-    console.log("Connected to the DB !!!!!!!!!!!",dbConnection.connection.host)
-    app.on("error", (error)=>{
-      console.log("Error in running server @@@:-",error.message)
+    await connectDb();
+    app.on("error", (error) => {
+      console.log("Error in running server @@@:-", error.message);
       throw error;
-    })
-
-    app.listen(process.env.PORT,()=>{
-      console.log("@@@ App is listening on port",process.env.PORT);
-
-    })
+    });
+    
+    app.listen(process.env.PORT, () => {
+      console.log("!!! App is listening on port", process.env.PORT);
+    });
   } catch (error) {
-    console.error("Error@@@@@@@@@", error);
-    throw error;
-    // return;
-    // process.exit(1);
+    console.log("Error in index.js:",error.message);
   }
 })();
 
+// console.log("object",process.env.MONGO_URI)
+// ;(async () => {
+//   try {
+//     const dbConnection = await mongoose.connect(process.env.MONGO_URI,{
+//       dbName:DB_NAME,
+//     })
+//     console.log("Connected to the DB !!!!!!!!!!!",dbConnection.connection.host)
+//     app.on("error", (error)=>{
+//       console.log("Error in running server @@@:-",error.message)
+//       throw error;
+//     })
+
+//     app.listen(process.env.PORT,()=>{
+//       console.log("@@@ App is listening on port",process.env.PORT);
+
+//     })
+//   } catch (error) {
+//     console.error("Error@@@@@@@@@", error);
+//     throw error;
+//     // return;
+//     // process.exit(1);
+//   }
+// })();
